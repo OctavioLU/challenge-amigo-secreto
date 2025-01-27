b187787416f51363b987e3db00caf54453a59b1f
Octavio Leon <octavioleon523@gmail.com>
2025-01-26 21:59:21 -0600

    crea la funcion agregar en lista

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
 let nombreAmigos = [];
+let lu = document.getElementById('listaAmigos');
+
+function limpiarInput(){
+    document.getElementById('amigo').value = '';
+    
+}
 
 function agregarAmigo(){
+
     let nombreIngresado = '';
     nombreIngresado = document.getElementById('amigo').value;
+   
     if (nombreIngresado == ''){
         alert ('Por favor, ingrese un nombre');
     }else{
         nombreAmigos.push(nombreIngresado);
     }
     console.log(nombreAmigos);
+    lu.innerHTML = '';
+    agregarEnLista(nombreAmigos);
     limpiarInput();
     return;
 }
-function limpiarInput(){
-    document.getElementById('amigo').value = '';
+
+function agregarEnLista(nombre){
+
+    //let lista = document.getElementById('listaAmigos');
+    //lista.innerHTML = nombre
+    for (let i = 0; i<nombreAmigos.length;i++){
+        
+        let elementoLista = document.createElement('li');
+        
+        let elementoDeLista = lu.appendChild(elementoLista);
+
+        elementoDeLista.textContent = nombre[i];
+        
+    }
+    return;
 }
