dde75a058525ad6ad8d291bd9f941ff0935245f4
Octavio Leon <octavioleon523@gmail.com>
2025-01-26 18:30:02 -0600

    crea funcion de validar entrada en array

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombreAmigos = [];
+
+function agregarAmigo(){
+    let nombreIngresado = '';
+    nombreIngresado = document.getElementById('amigo').value;
+    if (nombreIngresado == ''){
+        alert ('Por favor, ingrese un nombre');
+    }else{
+        nombreAmigos.push(nombreIngresado);
+    }
+    console.log(nombreAmigos);
+    limpiarInput();
+    return;
+}
+function limpiarInput(){
+    document.getElementById('amigo').value = '';
+}
